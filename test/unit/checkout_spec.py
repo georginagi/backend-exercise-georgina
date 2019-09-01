a8133326d25d3032fa71e5588265e694beb1284e
Georgina Giannoukou <g.giannoukou@gmail.com>
2019-09-01 12:22:15 +0300

    Green: Checkout, it calculates correct total amount when pricing rules do not exist

--- test/unit/checkout_spec.py
+++ test/unit/checkout_spec.py
@@ -15,3 +15,16 @@ with description('Checkout'):
         checkout.scan(item)
 
         expect(checkout.calculate_total()).to(equal(item.price))
+
+    with it('calculates correct total amount when pricing rules do not exist'):
+        voucher = Item('VOUCHER', 'Gift Card', Decimal(5.00))
+        t_shirt = Item('TSHIRT', 'Summer T-Shirt', Decimal(20.00))
+        pants = Item('PANTS', 'Summer Pants', Decimal(7.50))
+
+        checkout = Checkout(PricingRules())
+
+        checkout.scan(voucher)
+        checkout.scan(t_shirt)
+        checkout.scan(pants)
+
+        expect(checkout.calculate_total()).to(equal(Decimal(32.50)))
